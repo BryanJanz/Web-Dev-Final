5cfce100374ca1dbea9a75e0651c8fa3effd7797
Moomosa <113059275+Moomosa@users.noreply.github.com>
2023-04-10 15:23:03 -0600

    Everything is pretty now. Checkout page is complete.

--- 245Final/Pages/CartPage/Checkout.cshtml.cs
+++ 245Final/Pages/CartPage/Checkout.cshtml.cs
@@ -16,6 +16,7 @@ namespace _245Final.Pages.CartPage
 
         public CartViewModel Cart { get; set; }
         public List<SelectListItem> Provinces { get; set; }
+        public List<SelectListItem> Months { get; set; }
 
         public CheckoutModel(IHttpContextAccessor httpContextAccessor)
         {
@@ -41,6 +42,22 @@ namespace _245Final.Pages.CartPage
                 new SelectListItem { Value = "QC", Text = "Quebec" },
                 new SelectListItem { Value = "SK", Text = "Saskatchewan" },
                 new SelectListItem { Value = "YT", Text = "Yukon" }
+            };
+
+            Months = new()
+            {
+                new SelectListItem {Value = "01", Text = "January"},
+                new SelectListItem {Value = "02", Text = "February"},
+                new SelectListItem {Value = "03", Text = "March"},
+                new SelectListItem {Value = "04", Text = "April"},
+                new SelectListItem {Value = "05", Text = "May"},
+                new SelectListItem {Value = "06", Text = "June"},
+                new SelectListItem {Value = "07", Text = "July"},
+                new SelectListItem {Value = "08", Text = "August"},
+                new SelectListItem {Value = "09", Text = "September"},
+                new SelectListItem {Value = "10", Text = "October"},
+                new SelectListItem {Value = "11", Text = "November"},
+                new SelectListItem {Value = "12", Text = "December"}
             };
         }
 
